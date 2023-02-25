432ecd55e7664c9aa36a477afd8dd5252168eb26
youknowfred <81190215+youknowfred@users.noreply.github.com>
2023-02-25 15:41:46 -0500

    personal keymap

---
+++ keyboards/jasonhazel/bad_wings/keymaps/youknowfred/rules.mk
@@ -0,0 +1,11 @@
+# Bad Wings Specific
+TRACKPAD_ENABLE 		= yes
+
+# General QMK
+GRAVE_ESC_ENABLE		= no
+SPACE_CADET_ENABLE 	= no
+
+CAPS_WORD_ENABLE		= yes
+KEY_OVERRIDE_ENABLE	= yes
+COMBO_ENABLE				= yes
+TAP_DANCE_ENABLE		= yes
